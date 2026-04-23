c3cacb31a91c049cbaafb2706a6ebfdbc827a169
naomidagreatest <naomiller110@gmail.com>
2026-04-23 04:46:30 +0400

    test push 2

--- src/main/java/net/zenanaomi/desidelight/DesiDelight.java
+++ src/main/java/net/zenanaomi/desidelight/DesiDelight.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-//test push
+//test push 2
 @Mod(DesiDelight.MOD_ID)
 public class DesiDelight
 {
