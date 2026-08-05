f1af61a82d0b8a808ae5b0567bf7fec12ec810cd
naomidagreatest <naomiller110@gmail.com>
2026-08-05 22:38:53 +0400

    minor boat change

--- src/main/java/net/zenanaomi/desidelight/entity/ModEntities.java
+++ src/main/java/net/zenanaomi/desidelight/entity/ModEntities.java
@@ -13,8 +13,8 @@ import net.zenanaomi.desidelight.entity.custom.ModChestBoatEntity;
 public class ModEntities {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, DesiDelight.MOD_ID);
 
-    public static final RegistryObject<EntityType<ModBoatEntity>> DESI_DELIGHT_BOAT = ENTITY_TYPES.register("mod_boat", () -> EntityType.Builder.<ModBoatEntity>of(ModBoatEntity::new, MobCategory.MISC).sized(1.375f, 0.5625f).build("mod_boat"));
-    public static final RegistryObject<EntityType<ModChestBoatEntity>> DESI_DELIGHT_CHEST_BOAT = ENTITY_TYPES.register("mod_chest_boat", () -> EntityType.Builder.<ModChestBoatEntity>of(ModChestBoatEntity::new, MobCategory.MISC).sized(1.375f, 0.5625f).build("mod_chest_boat"));
+    public static final RegistryObject<EntityType<ModBoatEntity>> DESI_DELIGHT_BOAT = ENTITY_TYPES.register("desidelight_boat", () -> EntityType.Builder.<ModBoatEntity>of(ModBoatEntity::new, MobCategory.MISC).sized(1.375f, 0.5625f).build("desidelight_boat"));
+    public static final RegistryObject<EntityType<ModChestBoatEntity>> DESI_DELIGHT_CHEST_BOAT = ENTITY_TYPES.register("desidelight_chest_boat", () -> EntityType.Builder.<ModChestBoatEntity>of(ModChestBoatEntity::new, MobCategory.MISC).sized(1.375f, 0.5625f).build("desidelight_chest_boat"));
 
     public static void register(IEventBus eventBus) {
         ENTITY_TYPES.register(eventBus);
